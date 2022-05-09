66e02dc5dda07af26e6571795ffece3ce114e46d
Okama Innocent <73720386+thenewbio@users.noreply.github.com>
2022-05-09 22:39:39 +0100

    Update widget.dart

---
+++ widget.dart
@@ -0,0 +1,10 @@
+void main() {
+  stdout.write("What's your name? ");
+  String name = stdin.readLineSync();
+
+  print("Hi, $name! What is your age?");
+  int age = int.parse(stdin.readLineSync());
+
+  int yearsToHunderd = 100 - age;
+  print("$name, You have $yearsToHunderd years to be 100");
+}
